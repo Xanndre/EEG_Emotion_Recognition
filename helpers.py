aadc3eaea64114d7d074f805f4164b83a8310d28
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2021-04-04 13:39:00 +0200

    Add mapping emotion labels to arousal and valence classes

--- helpers.py
+++ helpers.py
@@ -18,3 +18,21 @@ def create_empty_pair_feature_df(pairs, bands):
                 col_name = pair[0] + '_' + pair[1] + '_' + band
                 col_names.append(col_name)
     return pd.DataFrame(columns=col_names)
+
+
+def emotion_to_arousal(emotion):
+    if emotion in [0, 2, 5]:
+        return 1
+    elif emotion in [4, 11]:
+        return 2
+    else:
+        return 3
+
+
+def emotion_to_valence(emotion):
+    if emotion in [1, 2, 3, 5, 12]:
+        return 1
+    elif emotion in [0, 6]:
+        return 2
+    else:
+        return 3
