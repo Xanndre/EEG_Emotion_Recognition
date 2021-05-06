1f32f2e34944489844f7233a6e089373c2e99f94
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2021-05-06 23:44:34 +0200

    Change channels' order

--- constants.py
+++ constants.py
@@ -1,11 +1,28 @@
-CHANNELS_32 = ['FP1', 'AF3', 'F3', 'F7', 'FC5', 'FC1', 'C3', 'T7',
-               'CP5', 'CP1', 'P3', 'P7', 'PO3', 'O1', 'OZ', 'PZ',
-               'FP2', 'AF4', 'FZ', 'F4', 'F8', 'FC6', 'FC2', 'CZ',
-               'C4', 'T8', 'CP6', 'CP2', 'P4', 'P8', 'PO4', 'O2']
+CHANNELS_50 = ['C4', 'FC1', 'PO3', 'F4', 'FC5', 'CZ', 'P7', 'O1', 'T8',
+               'CP2', 'O2', 'C3', 'T7', 'CP5', 'P3', 'OZ', 'PZ', 'FP2',
+               'AF4', 'FZ', 'FC6', 'FC2', 'CP6', 'P4', 'P8', 'PO4', 'F7', 'F3',
+               'AF3', 'F8', 'FP1', 'CP1', 'C6', 'C2', 'P1', 'CP4', 'P2',
+               'C1', 'C5', 'CP3', 'CPZ', 'TP7', 'CB2', 'F5', 'FC3', 'F1', 'F2', 'CB1', 'FPZ', 'F6']
 
-CHANNELS_28 = ['F3', 'F7', 'FC5', 'FC1', 'C3', 'T7', 'CP5', 'P3', 'P7', 'PO3', 'O1',
-               'OZ', 'PZ', 'FP2', 'AF4', 'FZ', 'F4', 'FC6', 'FC2', 'CZ', 'C4', 'T8',
-               'CP6', 'CP2', 'P4', 'P8', 'PO4', 'O2']
+CHANNELS_42 = ['C4', 'FC1', 'PO3', 'F4', 'FC5', 'CZ', 'P7', 'O1', 'T8',
+               'CP2', 'O2', 'C3', 'T7', 'CP5', 'P3', 'OZ', 'PZ', 'FP2',
+               'AF4', 'FZ', 'FC6', 'FC2', 'CP6', 'P4', 'P8', 'PO4', 'F7', 'F3',
+               'AF3', 'F8', 'FP1', 'CP1', 'C6', 'C2', 'P1', 'CP4', 'P2',
+               'C1', 'C5', 'CP3', 'CPZ', 'TP7']
+
+CHANNELS_37 = ['C4', 'FC1', 'PO3', 'F4', 'FC5', 'CZ', 'P7', 'O1', 'T8',
+               'CP2', 'O2', 'C3', 'T7', 'CP5', 'P3', 'OZ', 'PZ', 'FP2',
+               'AF4', 'FZ', 'FC6', 'FC2', 'CP6', 'P4', 'P8', 'PO4', 'F7', 'F3',
+               'AF3', 'F8', 'FP1', 'CP1', 'C6', 'C2', 'P1', 'CP4', 'P2']
+
+CHANNELS_32 = ['C4', 'FC1', 'PO3', 'F4', 'FC5', 'CZ', 'P7', 'O1', 'T8',
+               'CP2', 'O2', 'C3', 'T7', 'CP5', 'P3', 'OZ', 'PZ', 'FP2',
+               'AF4', 'FZ', 'FC6', 'FC2', 'CP6', 'P4', 'P8', 'PO4', 'F7', 'F3',
+               'AF3', 'F8', 'FP1', 'CP1']
+
+CHANNELS_28 = ['C4', 'FC1', 'PO3', 'F4', 'FC5', 'CZ', 'P7', 'O1', 'T8',
+               'CP2', 'O2', 'C3', 'T7', 'CP5', 'P3', 'OZ', 'PZ', 'FP2',
+               'AF4', 'FZ', 'FC6', 'FC2', 'CP6', 'P4', 'P8', 'PO4', 'F7', 'F3']
 
 CHANNELS_11 = ['C4', 'FC1', 'PO3', 'F4',
                'FC5', 'CZ', 'P7', 'O1', 'T8', 'CP2', 'O2']
@@ -30,26 +47,6 @@ CHANNELS_SEED = ['FP1', 'FPZ', 'FP2', 'AF3', 'AF4', 'F7', 'F5', 'F3',
                  'P1', 'PZ', 'P2', 'P4', 'P6', 'P8', 'PO7', 'PO5', 'PO3',
                  'POZ', 'PO4', 'PO6', 'PO8', 'CB1', 'O1', 'OZ', 'O2', 'CB2']
 
-CHANNELS_50 = ['FP1', 'FPZ', 'FP2', 'AF3', 'AF4', 'F7', 'F5', 'F3',
-               'F1', 'FZ', 'F2', 'F4', 'F6', 'F8', 'FC5', 'FC3'
-               'FC1', 'FC2', 'FC6', 'T7', 'C5',
-               'C3', 'C1', 'CZ', 'C2', 'C4', 'C6', 'T8', 'TP7', 'CP5', 'CP3',
-               'CP1', 'CPZ', 'CP2', 'CP4', 'CP6', 'P7', 'P3',
-               'P1', 'PZ', 'P2', 'P4', 'P8', 'PO3', 'PO4', 'CB1', 'O1', 'OZ', 'O2', 'CB2']
-
-CHANNELS_42 = ['FP1', 'FP2', 'AF3', 'AF4', 'F7', 'F3',
-               'FZ', 'F4', 'F8', 'FC5',
-               'FC1', 'FC2', 'FC6', 'T7', 'C5',
-               'C3', 'C1', 'CZ', 'C2', 'C4', 'C6', 'T8', 'TP7', 'CP5', 'CP3',
-               'CP1', 'CPZ', 'CP2', 'CP4', 'CP6', 'P7', 'P3',
-               'P1', 'PZ', 'P2', 'P4', 'P8', 'PO3', 'PO4', 'O1', 'OZ', 'O2']
-
-CHANNELS_37 = ['FP1', 'FP2', 'AF3', 'AF4', 'F7', 'F3',
-               'FZ', 'F4', 'F8', 'FC5',
-               'FC1', 'FC2', 'FC6', 'T7',
-               'C3', 'CZ', 'C2', 'C4', 'C6', 'T8', 'CP5',
-               'CP1', 'CP2', 'CP4', 'CP6', 'P7', 'P3',
-               'P1', 'PZ', 'P2', 'P4', 'P8', 'PO3', 'PO4', 'O1', 'OZ', 'O2']
 
 BANDS = {'Delta': (1, 4),
          'Theta': (4, 8),
